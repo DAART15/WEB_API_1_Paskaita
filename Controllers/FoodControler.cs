6b8cc8e361e67e15854b4a0e1d41e04ffdc25881
ramunasdapkus@gmail.com <ramunasdapkus@gmail.com>
2024-07-10 20:01:34 +0300

    added food class

---
+++ Controllers/FoodControler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_API_1_Paskaita.Controllers
+{
+    [Route("api/food")]
+    [ApiController]
+    public class FoodControler : ControllerBase
+    {
+        [HttpGet("all")]
+        public IEnumerable<string> GetAllFood()
+        {
+            var allFoods = new List<string>();
+            allFoods.Add("Pizza");
+            allFoods.Add("Tomatoes");
+            allFoods.Add("Potatoes");
+
+            return allFoods;
+        }
+    }
+}
---
+++ Controllers/Models/Food.cs
@@ -0,0 +1,23 @@
+﻿
+namespace WEB_API_1_Paskaita.Controllers.Models
+{
+    public class Food
+    {
+        public Food(int id, string name, string country, double weight)
+        {
+            this.id = id;
+            Name = name;
+            Country = country;
+            Weight = weight;
+            CreationDateTime = DateTime.Now;
+            ExpirationDateTime = DateTime.Now.AddDays(30);
+        }
+
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public double Weight { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public DateTime ExpirationDateTime { get; set; }
+    }
+}
